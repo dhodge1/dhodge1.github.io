3716c501f397f8522274184ade43398825a83898
David Hodge <dhodge1@gmail.com>
2022-02-24 16:54:47 -0500

    Deploy Next.js to gh-pages

---
+++ out/_next/static/HOVchqkC_Kg04waIJX0GH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-4d2a20487c55e63d.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
