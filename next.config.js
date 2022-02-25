b8c6970c8b62f375aa8f1e698ef1111990e1053c
David Hodge <dhodge1@gmail.com>
2022-02-24 23:10:36 -0500

    revert to jekyll

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: !debug ? '' : '',
-}
-
-module.exports = nextConfig
