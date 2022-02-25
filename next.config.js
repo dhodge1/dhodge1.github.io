a369fb4c81736f51f7b1609b95413cbb57e7e926
David Hodge <dhodge1@gmail.com>
2022-02-24 22:32:24 -0500

    Revert "Updates path prefixes"
    This reverts commit 2b94facb52291533260f48ccf9d19cf2c1188b24.

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const debug = process.env.NODE_ENV !== 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: !debug ? '/portfolio' : '',
-  assetPrefix: !debug ? '/portfolio/' : '',
+  assetPrefix: !debug ? '' : '',
 }
 
 module.exports = nextConfig
--- out/_next/static/YUmPZylASUpXlApR6Nrzh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/39f4d8aba4b66723.css","static/chunks/pages/index-4d2a20487c55e63d.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
